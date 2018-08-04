59c17f8b005d2d841830adcd5f7a3daf8d0864fc
Sylvester <metagumi@outlook.com>
2018-08-04 08:51:12 -0400

    Initial commit

---
+++ src/main/java/com/slyone/properties/FirstApp.java
@@ -0,0 +1,10 @@
+package com.slyone.properties;
+
+import java.util.Properties;
+
+public class FirstApp {
+    public static void main(String[] args) {
+
+        Properties prop = new Properties();
+    }
+}
